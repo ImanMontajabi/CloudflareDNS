fbeda3ff5ce3cb09fbd0dd236103a2158174868e
Iman <iman.montajabi@gmail.com>
2023-03-26 19:27:27 -0700

    delete bugs fixed

--- CloudflareDNS.py
+++ CloudflareDNS.py
@@ -58,28 +58,10 @@ class Worker(QObject):
             this_dns_record = subdomain.replace(f".{this_domain}", "")
             if this_dns_record == dns_record_name:
                 content = record['content']
+                self.progress.emit((i_num, subdomain, content,"succeed", ""))
                 i_num += 1
-                self.progress.emit((i_num - 1, subdomain, content,"succeed", ""))
             self.finished.emit()
-
-
-
-        """
-        i_num = 0
-        for i in range(len(data)):
-            subdomain = data[i]['name']
-            domain = data[i]['zone_name']
-            record_name = subdomain.replace(f".{domain}", "")
-            if record_name == ip_name:
-                content = data[i]['content']
-                i_num += 1"""
-        
-
-
-                #self.progress.emit((i_num - 1, subdomain, content,"succeed", ""))
-        
-        #self.finished.emit()
-        # ===================================================================================
+# ===================================================================================
     def create(self):
         def scan_to_iplist():
             with open(self.json_path_create, 'r') as f:
@@ -143,59 +125,36 @@ class Worker(QObject):
         self.finished.emit()
         # ===================================================================================
     def delete(self):
-        def listip():
-            with open('user_id.json', 'r') as json_file:
-                user_data = json.load(json_file)
-                
-            email = user_data['email']
-            api_token = user_data['api_token']
-            zone_id = user_data['zone_id']
-            ip_name = user_data["ip_dns_record"]
-            url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records'
-            headers = {
-            'X-Auth-Email': email,
-            'X-Auth-Key': api_token,
-            'Content-Type': 'application/json'
-            }
-            response = requests.request('GET', url, headers=headers)
-            data = response.json()['result']
-            ip = []
-            for i in range(len(data)):
-                a = data[i]['name']
-                b = data[i]['zone_name']
-                c = a.replace(f".{b}", "")
-                if c == ip_name:
-                    ip.append(data[i]['content'])
-            return ip
         with open('user_id.json', 'r') as json_file:
             user_data = json.load(json_file)
+                
         email = user_data['email']
         api_token = user_data['api_token']
         zone_id = user_data['zone_id']
-        ip_name = user_data["ip_dns_record"]
-        params_name = f'{user_data["ip_dns_record"]}.{user_data["domain"]}'
+        domain = user_data['domain']
+        dns_record_name = user_data["ip_dns_record"]
         url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records'
         headers = {
-            "Content-Type": "application/json",
-            "X-Auth-Email": email,
-            "X-Auth-Key": api_token
+        'X-Auth-Email': email,
+        'X-Auth-Key': api_token,
+        'Content-Type': 'application/json'
         }
-        params = {
-            "name": params_name,
-            "per_page": 100
-        }
-        response = requests.get(url, headers=headers, params=params)
-        dns_response_result = response.json()['result']
-        iplist = listip()
-        i = 0
-        for ip in iplist:
-            for record in dns_response_result:
-                if record["content"] == ip:
+        page_num = 1
+        i_num = 0    
+        while True:
+            params = {"page": page_num,"per_page": 100}
+            response = requests.request("GET", url, headers=headers, params=params)
+            data = response.json()["result"]
+            if not data:
+                break
+
+            for record in data:
+                if record["name"] == f"{dns_record_name}.{domain}":
                     url_del = f"{url}/{record['id']}"
-                    response = requests.delete(url_del, headers=headers)
-                    if response.status_code == 200:
-                        self.progress.emit((i, record["name"], record["content"], "deleted", ""))
-                        i += 1
+                    requests.delete(url_del, headers=headers)
+                    self.progress.emit((i_num, record["name"], record["content"], "deleted", ""))
+                    i_num += 1
+            page_num += 1
         self.finished.emit()
 #=============================================================================
 class CloudflareDNS(QMainWindow):
